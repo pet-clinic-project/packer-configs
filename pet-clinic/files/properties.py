32f648d2c10ac3a1c9ca301d1a22b42648150c72
Aswin1118 <aswinvj390@gmail.com>
2023-10-20 17:15:08 +0530

    updated file

--- pet-clinic/files/properties.py
+++ pet-clinic/files/properties.py
@@ -25,7 +25,7 @@ with open(file_path, 'r') as f:
 with open(consul_file_path, 'r') as f:
     consul_file = f.read()
 
-file_contents = file_contents.replace("spring.datasource.url=jdbc:mysql://localhost:3306/petclinic", f"spring.datasource.url={rds_endpoint}")
+file_contents = file_contents.replace("spring.datasource.url=jdbc:mysql://localhost:3306/petclinic", f"spring.datasource.url=jdbc:mysql://{rds_endpoint}")
 file_contents = file_contents.replace("spring.datasource.username=petclinic", f"spring.datasource.username={database_details['username']}")
 file_contents = file_contents.replace("spring.datasource.password=petclinic", f"spring.datasource.password={database_details['password']}")
 consul_file = consul_file.replace("encrypt = encrypt", f'encrypt = "{database_details["consul"]}"')
